bd0ca4f951995fd9187a5a9903a56c535fb0443e
apanfilov <apanfilov@extpoint.com>
2016-12-29 16:55:33 +0700

    copy DetailView from Yii, add formater in DataColumn, remove children BaseHtml

--- grid/DetailView.js
+++ grid/DetailView.js
@@ -3,35 +3,158 @@
 const Jii = require('jii');
 const React = require('react');
 const ReactView = require('jii-react/ReactView');
+const BooleanValidator = require('jii/validators/BooleanValidator.js');
+const InvalidConfigException = require('jii/exceptions/InvalidConfigException.js');
+const Formatter = require('jii/i18n/Formatter.js');
+const Model = require('jii/base/Model');
+const Inflector = require('jii/helpers/Inflector');
+const _cloneDeep = require('lodash/cloneDeep');
 
 class DetailView extends ReactView {
+    
+    init(){
+        super.init();
+
+        if (this.props.model === null) {
+            throw new InvalidConfigException('Please specify the "model" property.');
+        }
+
+        this.formatter = _cloneDeep(this.props.formatter);
+        if (this.formatter === null && Jii.app.hasComponent('formatter')) {
+            this.formatter = Jii.app.formatter;
+        } 
+        else if (this.formatter.length) {
+            this.formatter = Jii.createObject(this.formatter);
+        }
+        
+        if (!this.formatter instanceof Formatter) {
+            throw new InvalidConfigException('The "formatter" property must be either a Format object or a configuration array.');
+        }
+    }
 
     render() {
+        const attributes = this.normalizeAttributes();
+
         return (
-            <table className='table table-striped table-bordered'>
+            <table {...this.props.options}>
                 <tbody>
-                {this.props.model.attributes().map((attribute, index) => {
-                    let value = this.props.model.get(attribute);
-
-                    if(Jii.app.hasComponent('formatter') && this.props.model.isAttributeBoolean(attribute)){
-                        value = Jii.app.formatter.asBoolean(value);
-                    }
-
-                    return (
-                        <tr key={index}>
-                            <th>{attribute}</th>
-                            <td>{value}</td>
-                        </tr>
-                    );
-                })}
+                    {attributes.map((attribute, index) => {
+                        return this.renderAttribute(attribute, index);
+                    })}
                 </tbody>
             </table>
         );
     }
+
+    /**
+     * Renders a single attribute.
+     * @param {Array} attribute the specification of the attribute to be rendered.
+     * @param {number} index the zero-based index of the attribute in the [[attributes]] array
+     * @return string the rendering result
+     */
+    renderAttribute(attribute, index) {
+        return (
+            <tr key={index}>
+                <th {...attribute['captionOptions']}>{attribute['label']}</th>
+                <td {...attribute['contentOptions']}>{this.formatter.format(attribute['value'], attribute['format'])}</td>
+            </tr>
+        );
+    }
+
+    /**
+     * Normalizes the attribute specifications.
+     * @return {Array} attribute
+     */
+    normalizeAttributes() {
+        let attributes = _cloneDeep(this.props.attributes);
+
+        if (attributes === null) {
+            if (this.props.model instanceof Model) {
+                attributes = this.props.model.attributes();
+            }
+            else if (typeof(this.props.model) == 'object') {
+                attributes = Object.keys(this.props.model);
+            }
+            else {
+                throw new InvalidConfigException('The "model" property must be either an array or an object.');
+            }
+        }
+
+        attributes.map((attribute, i) => {
+            if (typeof(attribute) == 'string') {
+                const matches = /^([\w\.]+)(:(\w*))?(:(.*))?/.exec(attribute);
+                if (!matches.length) {
+                    throw new InvalidConfigException('The attribute must be specified in the format of "attribute", "attribute:format" or "attribute:format:label"');
+                }
+
+                attribute = {
+                    'attribute': matches[1],
+                    'format': matches[3] ? matches[3] : 'text',
+                    'label': matches[5] ? matches[5] : null,
+                };
+            }
+
+            if (typeof(attribute) !== 'object') {
+                throw new InvalidConfigException('The attribute configuration must be an array.');
+            }
+
+            if (attribute['visible'] === false) {
+                delete attributes[i];
+                return;
+            }
+
+            if (!attribute['format']) {
+                attribute['format'] = 'text';
+            }
+
+            if (attribute['attribute']) {
+                const attributeName = attribute['attribute'];
+
+                if (!attribute['label']) {
+                    attribute['label'] = this.props.model instanceof Model
+                        ? this.props.model.getAttributeLabel(attributeName)
+                        : Inflector.camel2words(attributeName, true);
+                }
+                if (!('value' in attribute)) {
+                    attribute['value'] = this.props.model instanceof Model
+                        ? this.props.model.get(attributeName)
+                        : this.props.model[attributeName];
+                }
+            }
+            else if (!attribute['label'] || !('value' in attribute)) {
+                throw new InvalidConfigException('The attribute configuration requires the "attribute" element to determine the value and display label.');
+            }
+
+            attributes[i] = attribute;
+        });
+
+        return attributes;
+    }
 }
 
 DetailView.defaultProps = {
-    model: null
+    /**
+     * @var {object} the data model whose details are to be displayed. This can be a [[Model]] instance,
+     * an associative array, an object that implements [[Arrayable]] interface or simply an object with defined
+     * accessible non-static properties.
+     */
+    model: null,
+    /**
+     * @var {Array} a list of attributes to be displayed in the detail view. Each array element
+     * represents the specification for displaying one particular attribute.
+     */
+    attributes: null,
+    /**
+     * @var {object} the HTML attributes for the container tag of this widget. The `tag` option specifies
+     * what container tag should be used. It defaults to `table` if not set.
+     */
+    options: {'className': 'table table-striped table-bordered detail-view'},
+    /**
+     * @var {object|Formatter} the formatter used to format model attribute values into displayable texts.
+     * This can be either an instance of [[Formatter]] or an configuration array for creating the [[Formatter]]
+     * instance. If this property is not set, the `formatter` application component will be used.
+     */
+    formatter: null
 };
 
 module.exports = DetailView;
--- helpers/Html.js
+++ helpers/Html.js
@@ -1,8 +1,146 @@
 'use strict';
 
-var BaseHtml = require('./BaseHtml');
+var _forIn = require('lodash/forIn');
+var _uniq = require('lodash/uniq');
+var _difference = require('lodash/difference');
+var _values = require('lodash/values');
 
-class Html extends BaseHtml{
+class Html{
+
+    /**
+     * Generates a hyperlink tag.
+     * @param {string} text link body.
+     * @param {object|string|null} url the URL for the hyperlink tag
+     * @param {object} options the tag options in terms of name-value pairs. These will be rendered as
+     * the attributes of the resulting tag.
+     * If a value is null, the corresponding attribute will not be rendered.
+     * @returns {XML} hyperlink tag
+     */
+    static a(text, url = null, options = []){
+        options = Html.cssClassToString(options);
+
+        if(url !== null){
+            if (typeof(url) == 'object' && url[0].length) {
+                url = url[0];
+
+                //normalize begin link
+                if(url[0] != '/' && url[0] != '#'){
+                    url = '/' + url;
+                }
+            }
+
+            //normalize end link
+            if(url[url.length - 1] != '/'){
+                url += '/';
+            }
+
+            options['href'] = url;
+        }
+
+        return <a {...options}>{text}</a>;
+    }
+
+    /**
+     * Converting object className in string
+     * @param {object} options
+     * @returns {*}
+     */
+    static cssClassToString(options){
+        if(options['class']){
+            const classNameNew = options['class'];
+            delete options['class'];
+            Html.addCssClass(options, classNameNew);
+        }
+
+        if(options['className'] && typeof(options['className']) == 'object'){
+            options['className'] = _values(options['className']).join(' ');
+        }
+
+        return options;
+    }
+
+    /**
+     * Adds a CSS class (or several classes) to the specified options.
+     * If the CSS class is already in the options, it will not be added again.
+     * If class specification at given options is an array, and some class placed there with the named (string) key,
+     * overriding of such key will have no effect. For example:
+     *
+     * ```php
+     * options = {'class': {'persistent': 'initial'}};
+     * Html.addCssClass(options, {'persistent': 'override'});
+     * console.log(options['class']); // outputs: {'persistent': 'initial'};
+     * ```
+     *
+     * @param {object} options the options to be modified.
+     * @param {string|object} className the CSS class(es) to be added
+     */
+    static addCssClass(options, className) {
+        if (options['className']) {
+            if (typeof(options['className']) == 'object') {
+                options['className'] = Html.mergeCssClasses(options['className'], className);
+            }
+            else {
+                const classes = options['className'].split('/\s+/').filter(x => x);
+                options['className'] = Html.mergeCssClasses(classes, className).join(' ');
+            }
+        } else {
+            options['className'] = className;
+        }
+    }
+
+
+
+    /**
+     * Merges already existing CSS classes with new one.
+     * This method provides the priority for named existing classes over additional.
+     * {object} existingClasses already existing CSS classes.
+     * {object} additionalClasses CSS classes to be added.
+     * @return {object} merge result.
+     */
+    static mergeCssClasses(existingClasses, additionalClasses) {
+        if(typeof(additionalClasses) == 'string'){
+            additionalClasses = [additionalClasses];
+        }
+
+        _forIn(additionalClasses, (className, key) => {
+            if (typeof(key) == 'number' && !(className in existingClasses)) {
+                existingClasses.push(className);
+            }
+            else if (!existingClasses[key]) {
+                existingClasses[key] = className;
+            }
+        });
+
+        return _uniq(_values(existingClasses));
+    }
+
+
+
+    /**
+     * Removes a CSS class from the specified options.
+     * @param {object} options the options to be modified.
+     * @param {string|object} className the CSS class(es) to be removed
+     */
+    static removeCssClass(options, className) {
+        if (options['className']) {
+            if (typeof(options['className']) == 'object') {
+                const classes = _difference(options['className'], className);
+                if (!classes || !classes.length) {
+                    delete options['className'];
+                } else {
+                    options['className'] = classes;
+                }
+            } else {
+                let classes = options['className'].split('/\s+/').filter(x => x);
+                classes = _difference(classes, className);
+                if (!classes || !classes.length) {
+                    delete options['className'];
+                } else {
+                    options['className'] = classes.join(' ');
+                }
+            }
+        }
+    }
 }
 
 module.exports = Html;
